b2ae5cfbc375f5178829c43d1ac5941aa8756a7b
Yu Heng <211491L@student.hci.edu.sg>
2025-08-17 20:28:48 +0800

    portfolio v2.0

---
+++ src/script/index.js
@@ -0,0 +1,19 @@
+function scrollToHero() {
+    const heroSection = document.querySelector('.hero');
+    heroSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToProjects() {
+    const projectsSection = document.querySelector('.projects');
+    projectsSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToResearch() {
+    const researchSection = document.querySelector('.research');
+    researchSection.scrollIntoView({ behavior: 'smooth' });
+}
+
+function scrollToAchievements() {
+    const achievementsSection = document.querySelector('.achievements');
+    achievementsSection.scrollIntoView({ behavior: 'smooth' });
+}
