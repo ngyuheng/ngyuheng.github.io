ab181931c49dd14ad139f8c6cd25d816bc8c5577
ngyuheng <ngyuheng08@gmail.com>
2025-08-19 22:27:14 +0800

    added dark mode

--- src/script/index.js
+++ src/script/index.js
@@ -17,3 +17,16 @@ function scrollToAchievements() {
     const achievementsSection = document.querySelector('.achievements');
     achievementsSection.scrollIntoView({ behavior: 'smooth' });
 }
+
+function toggleDarkMode() {
+    document.documentElement.classList.toggle('dark-mode');
+    const toggleButton = document.querySelector('.toggle-icon');
+
+    if (document.documentElement.classList.contains('dark-mode')) {
+        toggleButton.src = 'src/icons/light-mode.svg';
+    } else {
+        toggleButton.src = 'src/icons/dark-mode.svg';
+    }
+
+    localStorage.setItem('theme', document.documentElement.classList.contains('dark-mode') ? 'dark' : 'light');
+}
